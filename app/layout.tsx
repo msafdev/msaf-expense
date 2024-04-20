82b87dd33e447f5d21b850ffe7f080f9c8624dfa
salman261002 <salmanalfarisi261002@gmail.com>
2024-04-20 21:46:51 +0700

    add meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { GeistSans } from "geist/font/sans";
+import Head from "next/head";
 import "./globals.css";
 
 import { Toaster } from "@/components/ui/toaster";
@@ -11,7 +12,6 @@ export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
-  content: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport" />
+      </Head>
       <body className="dark bg-background text-foreground min-h-screen flex flex-col items-center w-full">
         <main className="flex flex-col w-full flex-1">{children}</main>
         <Toaster />
